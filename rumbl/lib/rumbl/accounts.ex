8b798cd52de97eb6d640abef783a243325716fb3
iangelmx <aramireza1506@alumno.ipn.mx>
2020-10-25 20:21:26 -0600

    First approach with data on memory

---
+++ rumbl/lib/rumbl/accounts.ex
@@ -0,0 +1,30 @@
+defmodule Rumbl.Accounts do
+
+  @moduledoc """
+    The Accounts context.
+    """
+
+  alias Rumbl.Accounts.User
+
+  @spec list_users :: list( %User{} )
+  def list_users do
+    [
+      %User{id: "1", name: "José", username: "josevalim"},
+      %User{id: "2", name: "Bruce", username: "redrapids"},
+      %User{id: "3", name: "Chris", username: "chrismccord"}
+    ]
+  end
+
+  @spec get_user( String.t() ) :: %User{}
+  def get_user(id) do
+    Enum.find(list_users(), fn map -> map.id == id end)
+  end
+
+  @spec get_user_by(any) :: any
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
+
+end
